3fe7d7f69b23e2235fde2740807ba8b4745bbb16
Helena <a25helenams@iessanclemente.net>
2026-04-17 19:36:00 +0200

    Refactor

--- src/Buscaminas.kt
+++ src/Buscaminas.kt
@@ -46,58 +46,60 @@ data class Tablero(var filas: Int, var columnas: Int, var minas : Int){
             }
         }
     }
+}
+
+class Buscaminas(var tablero : Tablero) {
     private fun revelarCasillas(f: Int, c: Int) {
-        if (f !in arrayCeldas.indices || c !in arrayCeldas[f].indices) return // Si no esta en rango, salir
-        if (arrayCeldas[f][c].visibilidad != '?') return // Si ya fue revelada, salir
+        if (f !in tablero.arrayCeldas.indices || c !in tablero.arrayCeldas[f].indices) return // Si no esta en rango, salir
+        if (tablero.arrayCeldas[f][c].visibilidad != '?') return // Si ya fue revelada, salir
 
-        arrayCeldas[f][c].visibilidad = arrayCeldas[f][c].proximidad.toString().first() // Revelar casilla actual
+        tablero.arrayCeldas[f][c].visibilidad = tablero.arrayCeldas[f][c].proximidad.toString().first() // Revelar casilla actual
 
-        if (arrayCeldas[f][c].proximidad != 0) return // Si NO es 0, no expandir
+        if (tablero.arrayCeldas[f][c].proximidad != 0) return // Si NO es 0, no expandir
 
         // Expandir vecinos
         for (filas in f - 1..f + 1) {
             for (columnas in c - 1..c + 1) {
-                if (filas in arrayCeldas.indices && columnas in arrayCeldas[filas].indices) {
+                if (filas in tablero.arrayCeldas.indices && columnas in tablero.arrayCeldas[filas].indices) {
                     revelarCasillas(filas, columnas)
                 }
             }
         }
     }
-
     fun revelarMinas(){
-        for (f in 0 until arrayCeldas.size) {
-            for (c in 0 until arrayCeldas[f].size) {
-                if (arrayCeldas[f][c].proximidad == 9) {
-                    arrayCeldas[f][c].visibilidad = 'X'
+        for (f in 0 until tablero.arrayCeldas.size) {
+            for (c in 0 until tablero.arrayCeldas[f].size) {
+                if (tablero.arrayCeldas[f][c].proximidad == 9) {
+                    tablero.arrayCeldas[f][c].visibilidad = 'X'
                 }
             }
         }
     }
-    fun verificarVictoria(): Boolean{
-        var contador = 0
-        for (f in 0 until arrayCeldas.size) {
-            for (c in 0 until arrayCeldas[f].size) {
-                if (arrayCeldas[f][c].visibilidad == '?' || arrayCeldas[f][c].visibilidad == 'B')
-                    contador++
-            }
-        }
-        return contador == minas
-    }
     fun abriCasilla(f: Int, c: Int ): Boolean{
-        if (arrayCeldas[f][c].proximidad != 9){
+        if (tablero.arrayCeldas[f][c].proximidad != 9){
             revelarCasillas(f,c)
             return true
         }
         else {
-            arrayCeldas[f][c].visibilidad = 'X'
+            tablero.arrayCeldas[f][c].visibilidad = 'X'
             return false
         }
     }
-
-
-}
-
-class Buscaminas(var tablero : Tablero) {
-
+    fun colocarBandera(f: Int,c: Int): Boolean{
+        if (tablero.arrayCeldas[f][c].visibilidad == '?'){
+            tablero.arrayCeldas[f][c].visibilidad = 'B'
+        }
+        return true
+    }
+    fun verificarVictoria(): Boolean{
+        var contador = 0
+        for (f in 0 until tablero.arrayCeldas.size) {
+            for (c in 0 until tablero.arrayCeldas[f].size) {
+                if (tablero.arrayCeldas[f][c].visibilidad == '?' || tablero.arrayCeldas[f][c].visibilidad == 'B')
+                    contador++
+            }
+        }
+        return contador == tablero.minas
+    }
 
 }
--- src/Main.kt
+++ src/Main.kt
@@ -4,38 +4,26 @@ class Menu() {
     val AMARILLO = "\u001B[33m"
     val RESET = "\u001B[0m"
     val AZUL = "\u001B[34m"
-
-    fun leerEntero(mensaje: String, tipo: String, min: Int, max: Int): Int {
-        while (true) {
-            print(mensaje)
-            val valor = readlnOrNull()?.toIntOrNull()
-
-            if (valor != null && valor in min..max) {
-                return valor
-            }
-
-            throw IllegalArgumentException("Error! numero de $tipo invalido. Debe estar entre $min y $max.")
-        }
-    }
     fun parametrosConfiguracion(): Triple<Int, Int, Int> {
-
+        var entero = EntradaValidacion()
         println("=== Configuración del Buscaminas ===")
 
-        val filas = leerEntero("Introduce filas (1-8): ", "filas", 1, 8)
-        val columnas = leerEntero("Introduce columnas (1-8): ", "columnas", 1, 8)
+        val filas = entero.leerEntero("Introduce filas (1-8): ", "filas", 1, 8)
+        val columnas = entero.leerEntero("Introduce columnas (1-8): ", "columnas", 1, 8)
 
         val totalCasillas = filas * columnas
 
-        val minas = leerEntero("Introduce minas (1-$totalCasillas): ", "minas", 1, totalCasillas - 1)
+        val minas = entero.leerEntero("Introduce minas (1-$totalCasillas): ", "minas", 1, totalCasillas - 1)
 
         return Triple(filas, columnas, minas)
     }
     fun pedirAccion(filas: Int, columnas: Int): Triple<String, Int, Int> {
+        var entrada = EntradaValidacion()
         // 1. Preguntar al usuario qué quiere hacer
         println("¿Qué deseas hacer?")
-        var option = leerEntero("1. Abrir celda.\n2. Poner bandera\n", "opcion", 1, 2)
-        var fila = leerEntero("Introduce la coordenada que corresponde a la fila: ", "fila", 0, filas - 1)
-        var columna = leerEntero("Introduce la coordenada que corresponde a la columna: ", "columna", 0, columnas - 1)
+        var option = entrada.leerEntero("1. Abrir celda.\n2. Poner bandera\n", "opcion", 1, 2)
+        var fila = entrada.leerEntero("Introduce la coordenada que corresponde a la fila: ", "fila", 0, filas - 1)
+        var columna = entrada.leerEntero("Introduce la coordenada que corresponde a la columna: ", "columna", 0, columnas - 1)
 
         return Triple(if (option == 1) "Abrir" else "Bandera", fila, columna)
     }
@@ -49,24 +37,63 @@ class Menu() {
                     print(ROJO + "[${buscaminas.tablero.arrayCeldas[f][c].visibilidad}]" + RESET)
                 }
                 else if (
+                    buscaminas.tablero.arrayCeldas[f][c].visibilidad == 'B'
+                ) {
+                    print(AZUL + "[${buscaminas.tablero.arrayCeldas[f][c].visibilidad}]" + RESET)
+                }
+                else if (
                     buscaminas.tablero.arrayCeldas[f][c].visibilidad != '0' &&
                     buscaminas.tablero.arrayCeldas[f][c].visibilidad != '?'
                 ) {
                     print(AMARILLO + "[${buscaminas.tablero.arrayCeldas[f][c].visibilidad}]" + RESET)
-                }  else print("[${buscaminas.tablero.arrayCeldas[f][c].visibilidad}]")
+                }
+                else print("[${buscaminas.tablero.arrayCeldas[f][c].visibilidad}]")
             }
             println()
         }
     }
-    fun sentenciar(sentencia: String){
-        when (sentencia){
-            "Revelando" -> {println(AZUL + sentencia + "..." + RESET)}
-            "Victoria"  -> {println(VERDE + sentencia +"!!!"+ RESET)}
-            "Game Over" -> {println(ROJO + sentencia +" :( "+ RESET)}
-            else        -> {println(sentencia)}
-        }
-
+    fun jugar(juego: Buscaminas, filas: Int, columnas: Int){
+        do {
+            var (opcion,fila,columna) = pedirAccion(filas,columnas)
+            var continuar: Boolean
+            var victoria  = true
+            if (opcion=="Abrir") {
+                continuar = juego.abriCasilla(fila,columna)
+                if (continuar){
+                    println(AZUL + "Revelando" + RESET)
+                }
+                else {
+                    println(ROJO + "Game Over" + RESET )
+                    juego.revelarMinas()
+                }
+                imprimirTablero(juego)
+                victoria = juego.verificarVictoria()
+                if (victoria){
+                    println(VERDE + "Victoria" + RESET)
+                }
+            }
+            else {
+                println(AZUL + "Colocando Bandera" + RESET)
+                continuar = juego.colocarBandera(fila,columna)
+                imprimirTablero(juego)
+            }
+        } while(continuar && !victoria )
     }
+}
+class EntradaValidacion(){
+    fun leerEntero(mensaje: String, tipo: String, min: Int, max: Int): Int {
+        while (true) {
+            print(mensaje)
+            val valor = readlnOrNull()?.toIntOrNull()
+
+            if (valor != null && valor in min..max) {
+                return valor
+            }
+
+            throw IllegalArgumentException("Error! numero de $tipo invalido. Debe estar entre $min y $max.")
+        }
+    }
+
 }
 fun main() {
     var menu = Menu()
@@ -74,26 +101,7 @@ fun main() {
     var tablero = Tablero(filas,columnas,minas)
     var juego = Buscaminas(tablero)
     menu.imprimirTablero(juego)
-    do {
-        var (opcion,fila,columna) = menu.pedirAccion(filas,columnas)
-        var continuar = juego.tablero.abriCasilla(fila,columna)
-        var victoria  = true
-        if (continuar){
-            menu.sentenciar("Revelando")
-        }
-        else {
-            menu.sentenciar("Game Over")
-            juego.tablero.revelarMinas()
-        }
-        menu.imprimirTablero(juego)
-        victoria = juego.tablero.verificarVictoria()
-        if (victoria){
-            menu.sentenciar("Victoria")
-        }
-    } while(continuar && !victoria )
-
-
-
+    menu.jugar(juego,filas,columnas)
 
 
 
